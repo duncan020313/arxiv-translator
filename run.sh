0b5ba279a8da460a78ca8fef969cfc07dd151ef0
duncan020313 <milkbear0313@gmail.com>
2024-01-22 10:32:07 +0900

    2212.11870

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+#bin/zsh
 ARXIV_ID=$1
 
 if [ -z "$ARXIV_ID" ]; then
@@ -6,23 +7,23 @@ if [ -z "$ARXIV_ID" ]; then
 fi
 
 # Ready the directory
-rm -rf papers/$ARXIV_ID
-mkdir -p papers/$ARXIV_ID
+rm -rf ./papers/$ARXIV_ID
+mkdir -p ./papers/$ARXIV_ID
 
 # Download the paper
-paper $ARXIV_ID -p -d papers/$ARXIV_ID
-PDF_FN=$(ls -1 papers/$ARXIV_ID/*.pdf | head -n 1)
+paper $ARXIV_ID -p -d ./papers/$ARXIV_ID
+PDF_FN=$(ls -1 ./papers/$ARXIV_ID/*.pdf | head -n 1)
 echo $PDF_FN
 
 # If the directory is empty, remove it
 # Exit if the directory is empty
-if [ ! "$(ls -A papers/$ARXIV_ID)" ]; then
-    rm -rf papers/$ARXIV_ID
+if [ ! "$(ls -A ./papers/$ARXIV_ID)" ]; then
+    rm -rf ./papers/$ARXIV_ID
     exit 1
 fi
 
 # Extract the text
-nougat $PDF_FN -o papers/$ARXIV_ID
+nougat $PDF_FN -o ./papers/$ARXIV_ID
 MMD_FN=$(echo $PDF_FN | sed 's/pdf/mmd/g')
 echo $MMD_FN
 
